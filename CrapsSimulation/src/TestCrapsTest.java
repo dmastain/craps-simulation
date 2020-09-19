5ba9ecff84c6ca6c2637c3cf8b1b6e9b40d463ce
Daniel Mastain <dmastain@gmail.com>
2020-09-18 20:23:35 -0500

    Addded two JUnit 5 test for "no static overide"

---
+++ CrapsSimulation/src/TestCrapsTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCrapsTest
+{
+
+	@Test
+	void test_static_methods_dont_override()
+	{
+		Die die = new CrookedDie1();
+		
+		assertEquals("Die.testStatic()", die.testStatic(), "error for this");
+	}
+
+	@Test
+	void test_downcasting_doesnt_change_static_lookup()
+	{
+		Die die = new CrookedDie1();
+		
+		assertEquals("CrookedDie1.testStatic()", ((CrookedDie1)die).testStatic());
+	}
+}
